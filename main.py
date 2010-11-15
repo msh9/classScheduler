0519b0adb400ab9d9e6a6d8032876fa1ea3aae27
msh9 <msh9@uw.washington.edu>
2010-11-15 13:06:56 -0800

    main.py: Moved option parsing into a seperate function that returns the parsed options pyparsing_py3.py: Added Paul McGuire's pyparsing library to the project so we can make grammar/tokenizers to parse information out of input files

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ and prerequisite information"""
 from optparse import OptionParser
 import os.path
 
-def main():
-    """Main application entry point, args should be parsed by 
-    optparser
-    
-    Returns an int indicating exit status
+def get_options():
+    """Uses an OptionParser to parse command line options
+    Returns a python dict of str -> file if successful. Prints an error and returns 1
+    otherwise
     """
     parser = OptionParser()
     # default action = store and default type check = string
@@ -25,14 +24,14 @@ def main():
     (options,args) = parser.parse_args()
     # initialize a dictionary to hold files
     files = {}
-    # by default we won't take summer classes
-    summer_classes = False
     # the next piece is messy, essentially we check that each file ame was
     # specified and is a valid file name. If so then we open the files and
     # store the file handles in the files dictionary
     if (options.summer is not None and os.path.isfile(options.summer)):
-        summer_classes = True
         files['summer'] = open(options.summer) #default open mode is read only
+    else:
+        print("Summer classes were not specified")
+        return 1
     if (options.fall is not None and os.path.isfile(options.fall)):
         files['fall'] = open(options.fall)
     else:
@@ -53,9 +52,22 @@ def main():
     else:
         print("Prereqs were not specified")
         return 1
-     
-    print(str(files))
-    return 0
+    return files
+
+def main():
+    """Main application entry point, args should be parsed by 
+    optparser
+    
+    Returns an int indicating exit status
+    """
+    # get options
+    files = get_options()
+    # check if we had an error
+    if (files == 1) {
+        return 1
+    }
+    # if no error continue executing
+    
     
 if __name__ == "__main__":
     exit(main())
