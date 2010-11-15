f1fea90bc851e61f2c3598a58225f2d7c549a8ba
Michael Hughes <msh9@u.washington.edu>
2010-11-15 09:12:13 -0800

    Added a simple option parser to main

--- main.py
+++ main.py
@@ -2,15 +2,30 @@
 and prerequisite information"""
 
 from optparse import OptionParser
+import os.path
 
 def main():
     """Main application entry point, args should be parsed by 
     optparser
+    
+    Returns an int indicating exit status
     """
+    parser = OptionParser()
+    # default action = store and default type check = string
+    parser.add_option("-w","--winter", 
+        help="Winter Classes")
+    parser.add_option("-s","--spring",  
+        help="Spring Classes")
+    parser.add_option("-f","--fall",
+        help="Fall Classes")
+    parser.add_option("-p","--prereqs", 
+        help="Class Prerequisites")
+    (options,args) = parser.parse_args()
+    print(options.winter)
     
     
     
     
     
-if __name__ == "main":
+if __name__ == "__main__":
     exit(main())
