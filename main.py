1dabb5061abf951cb583fd23d97c155769d7f32b
unknown <michael@.(none)>
2010-11-15 12:45:35 -0800

    Completed initial pass of option parsing code

--- main.py
+++ main.py
@@ -14,18 +14,48 @@ def main():
     # default action = store and default type check = string
     parser.add_option("-w","--winter", 
         help="Winter Classes")
-    parser.add_option("-s","--spring",  
+    parser.add_option("-g","--spring",  
         help="Spring Classes")
     parser.add_option("-f","--fall",
         help="Fall Classes")
     parser.add_option("-p","--prereqs", 
         help="Class Prerequisites")
+    parser.add_option("-s","--summer",
+        help="Summer Classes")
     (options,args) = parser.parse_args()
-    print(options.winter)
-    
-    
-    
-    
+    # initialize a dictionary to hold files
+    files = {}
+    # by default we won't take summer classes
+    summer_classes = False
+    # the next piece is messy, essentially we check that each file ame was
+    # specified and is a valid file name. If so then we open the files and
+    # store the file handles in the files dictionary
+    if (options.summer is not None and os.path.isfile(options.summer)):
+        summer_classes = True
+        files['summer'] = open(options.summer) #default open mode is read only
+    if (options.fall is not None and os.path.isfile(options.fall)):
+        files['fall'] = open(options.fall)
+    else:
+        print("Fall classes were not specified")
+        return 1
+    if (options.winter is not None and os.path.isfile(options.winter)):
+        files['winter'] = open(options.winter)
+    else:
+        print("Winter classes were not specified")
+        return 1
+    if (options.spring is not None and os.path.isfile(options.spring)):
+        files['spring'] = open(options.spring)
+    else:
+        print("Spring classes were not specified")
+        return 1
+    if (options.prereqs is not None and os.path.isfile(options.prereqs)):
+        files['prereqs'] = open(options.prereqs)
+    else:
+        print("Prereqs were not specified")
+        return 1
+     
+    print(str(files))
+    return 0
     
 if __name__ == "__main__":
     exit(main())
