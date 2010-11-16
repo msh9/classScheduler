3772b6dfb1bd94de4f0d9d6b51beae401f33b0cd
Michael Hughes <msh9@u.washington.edu>
2010-11-15 18:05:20 -0800

    Added code in main() to parse quarterly class offering files for classes offered per quarter and credit amount

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ and prerequisite information"""
 
 from optparse import OptionParser
 import os.path
+import Class
 
 def get_options():
     """Uses an OptionParser to parse command line options
@@ -64,12 +65,20 @@ def parse_normal_form(reqs):
         A | (B & C) will become [ (A), (B, C) ]
     """
     final = []
+    # first split into list of disjunctions
     conjunctions = reqs.split("|")
+    # for each conjunctive term...
     for conjunction in conjunctions:
         curr_set = None
+        # are we dealing with a bunch of terms together?
         if conjunction.strip().startswith('('):
+            # if so we...get ready...strip off space at either end of the term, then strip off
+            # then ( ), then we split on &, then we use a map to take the split list to one where
+            # all elements have had the trailing and leading white space stripped, then finally
+            # we put the final list into a frozen(constant)set
             curr_set = frozenset(map(lambda x: x.strip(),conjunction.strip()[1:-1].split('&')))
         else:
+            # or if we have a single term we just put it into a singleton frozenset...
             curr_set = frozenset([conjunction.strip()])
         final.append(curr_set)   
     return final
@@ -86,6 +95,29 @@ def main():
     if files == 1:
         return 1
     # if no error continue executing
+    # create a set of all classes we will ever consider (i.e. the union of all
+    # sets of classes offered every quarter)
+    quarters = ['fall','winter','spring']
+    if 'summer' in files.keys():
+        quarters.append('summer')
+    all_classes = {}
+    i = 0 
+    for qtr in quarters:
+        for line in files[qtr]:
+            (cls, credits) = line.split(':')
+            try:
+                # subscript madness: cls indexes into the dictionary by the class name key,
+                # then we index into the first item in the list, and then finally index into
+                # the offered tuple by quarter i
+                all_classes[cls][0][i] = True
+            except KeyError:
+                offered = [False,False,False,False]
+                offered[i] = True
+                all_classes[cls] = [offered, int(credits)]
+            else:
+                 
+                all_classes[cls][0][i] = True
+        i += 1
     
     
 if __name__ == "__main__":
